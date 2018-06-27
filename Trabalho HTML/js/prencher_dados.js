694ed0a557298b8346e4bf0a6c6905e2fa03c3f5
TiagoERodrigues <40641534+TiagoERodrigues@users.noreply.github.com>
2018-06-27 15:19:23 -0300

    Add files via upload

---
+++ Trabalho HTML/js/prencher_dados.js
@@ -0,0 +1,100 @@
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mcc(v){
+    v=v.replace(/\D/g,"");
+    v=v.replace(/^(\d{4})(\d)/g,"$1 $2");
+    v=v.replace(/^(\d{4})\s(\d{4})(\d)/g,"$1 $2 $3");
+    v=v.replace(/^(\d{4})\s(\d{4})\s(\d{4})(\d)/g,"$1 $2 $3 $4");
+    return v;
+}
+function id( el ){
+	return document.getElementById( el );
+}
+window.onload = function(){
+	id('cc').onkeypress = function(){
+		mascara( this, mcc );
+	}
+}
+//---------------------------------------------------------------------------
+function PagamentoFuncao() {
+  var cartao = document.getElementById("opt-cartao").checked;
+  if (cartao) {
+    document.getElementById("cartao").style.display = "block";
+    document.getElementById("boleto").style.display = "none";
+  } else {
+    document.getElementById("cartao").style.display = "none";
+    document.getElementById("boleto").style.display = "block";
+  }
+}
+
+//--------------------------------------------------------------------------
+function limpa_formulário_cep() {
+            //Limpa valores do formulário de cep.
+            document.getElementById('rua').value=("");
+            document.getElementById('bairro').value=("");
+            document.getElementById('cidade').value=("");
+            document.getElementById('uf').value=("");
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('rua').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor.replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('rua').value="...";
+                document.getElementById('bairro').value="...";
+                document.getElementById('cidade').value="...";
+                document.getElementById('uf').value="...";
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
